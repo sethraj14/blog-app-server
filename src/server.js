9f5d8cdcb59cf9e8e063b28b05d020df859536ee
Rajdeep Gupta <rajdeepseth007@gmail.com>
2024-02-15 01:32:17 +0530

    Feat: Added vercel deployment with Github Actions

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ async function startApolloServer(typeDefs, resolvers) {
     await server.start();
     server.applyMiddleware({ app });
 
-    const PORT = process.env.PORT || 4000;
+    const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}${server.graphqlPath}`));
 }
 
